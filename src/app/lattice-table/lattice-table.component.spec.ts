e23a175e3f809df6c8ec6a35a45e56382b788d08
Patrick Smith <pgwsmith@gmail.com>
2020-06-25 08:50:56 +1000

    Get component tests passing

--- src/app/lattice-table/lattice-table.component.spec.ts
+++ src/app/lattice-table/lattice-table.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LatticeTableComponent } from './lattice-table.component';
+import { TableData } from '../table-data';
+import { EvaluatorEnvironment, CellEvaluator } from '../evaluator.service';
 
 describe('LatticeTableComponent', () => {
   let component: LatticeTableComponent;
@@ -8,14 +10,24 @@ describe('LatticeTableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LatticeTableComponent ]
-    })
-    .compileComponents();
+      declarations: [LatticeTableComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LatticeTableComponent);
     component = fixture.componentInstance;
+
+    // component.data = { cellValues: new Map() };
+    component.data = new TableData();
+    component.evaluatorService = {
+      evaluatorForEnvironment(
+        environment: EvaluatorEnvironment
+      ): CellEvaluator {
+        return (cellID) => null;
+      },
+    };
+
     fixture.detectChanges();
   });
 
