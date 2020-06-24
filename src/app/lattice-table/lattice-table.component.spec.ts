12974382b2d1c47ca395c85725dbb6e3d0aea2da
Patrick Smith <pgwsmith@gmail.com>
2020-06-25 09:28:58 +1000

    Add Testing Library and test table component

--- src/app/lattice-table/lattice-table.component.spec.ts
+++ src/app/lattice-table/lattice-table.component.spec.ts
@@ -1,10 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { render, screen } from '@testing-library/angular';
+import user from '@testing-library/user-event';
 
 import { LatticeTableComponent } from './lattice-table.component';
 import { TableData } from '../table-data';
 import { EvaluatorEnvironment, CellEvaluator } from '../evaluator.service';
 
-describe('LatticeTableComponent', () => {
+// Here we compare Angular’s built-in TestBed, and ‘Testing Library’, which helps with querying the page.
+
+describe('LatticeTableComponent using TestBed', () => {
   let component: LatticeTableComponent;
   let fixture: ComponentFixture<LatticeTableComponent>;
 
@@ -20,7 +24,7 @@ describe('LatticeTableComponent', () => {
 
     component.data = new TableData({
       columnCount: 17,
-      rowCount: 21
+      rowCount: 21,
     });
     component.evaluatorService = {
       evaluatorForEnvironment(
@@ -42,6 +46,42 @@ describe('LatticeTableComponent', () => {
   });
 
   it('shows 21 rows', () => {
-    expect(fixture.nativeElement.querySelectorAll('tbody tr').length).toBe(21)
+    expect(fixture.nativeElement.querySelectorAll('tbody tr').length).toBe(21);
+  });
+});
+
+describe('LatticeTableComponent using Testing Library', () => {
+  beforeEach(async () => {
+    await render(LatticeTableComponent, {
+      componentProperties: {
+        data: new TableData({
+          columnCount: 17,
+          rowCount: 21,
+        }),
+        evaluatorService: {
+          evaluatorForEnvironment(
+            environment: EvaluatorEnvironment
+          ): CellEvaluator {
+            return (cellID) => null;
+          },
+        },
+      },
+    });
+  });
+
+  it('shows table', () => {
+    expect(screen.getByRole('table')).toBeTruthy();
+  });
+
+  it('shows 17 column headers', () => {
+    expect(screen.getAllByRole('columnheader').length).toBe(17);
+  });
+
+  it('shows 21 row headers', () => {
+    expect(screen.getAllByRole('rowheader').length).toBe(21);
+  });
+
+  it('shows 17 * 21 textbox inputs', () => {
+    expect(screen.getAllByRole('textbox').length).toBe(17 * 21);
   });
 });
